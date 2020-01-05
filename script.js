2d595ad610c6bcf10fbdc891657cba96647c7678
Sam Fuchs <samcfuchs@gmail.com>
2020-01-05 00:50:04 -0500

    Add third robot and description

--- script.js
+++ script.js
@@ -55,8 +55,11 @@ var robot1 = group.append("circle")
 
 var robot2 = group.append("circle")
     .attr("cy", PIVOT.y + PIVOT_TO_RUNG)
-    .text("2")
     .attr("id", "robot2");
+
+var robot3 = group.append("circle")
+    .attr("cy", PIVOT.y + PIVOT_TO_RUNG)
+    .attr("id", "robot3");
 
 var floor = svg.append("line")
     .attr("x1", 0)
@@ -66,16 +69,16 @@ var floor = svg.append("line")
     .attr("stroke", "black");
 
 var text = svg.append("text")
-    .attr("x", 20)
-    .attr("y", 20)
+    .attr("x", 30)
+    .attr("y", 50)
     .style("font-family", "Segoe UI")
     .style("fill", "black")
-    .style("font-size", "18px")
+    .style("font-size", "30px")
     .text("0°");
 
-function getAngle(m_1, d_1, m_2, d_2) {
-    let numerator = (m_1*d_1) + (m_2*d_2);
-    let denominator = (m_1*PIVOT_TO_RUNG_FT) + (m_2*PIVOT_TO_RUNG_FT) + (MASS*CENTER_OF_MASS_OFFSET_FT);
+function getAngle(m_1, d_1, m_2, d_2, m_3, d_3) {
+    let numerator = (m_1*d_1) + (m_2*d_2) + (m_3*d_3);
+    let denominator = (m_1*PIVOT_TO_RUNG_FT) + (m_2*PIVOT_TO_RUNG_FT) + (m_3*PIVOT_TO_RUNG_FT) + (MASS*CENTER_OF_MASS_OFFSET_FT);
     let theta = Math.atan(numerator / denominator)
     return theta;
 }
@@ -89,52 +92,35 @@ function setAngle(angle) {
 
     group.transition()
         .duration(750)
-        .attr("transform", `rotate(${theta} ${PIVOT.x} ${PIVOT.y})`);
-}
-
-function init() {
-    let m1 = d3.select("input[name=robot_1]").node().value;
-    let d1 = d3.select("input[name=robot_1_d]").node().value;
-    let m2 = d3.select("input[name=robot_2]").node().value;
-    let d2 = d3.select("input[name=robot_2_d]").node().value;
-
-    let angle = rad_to_deg(getAngle(m1, d1 / 12.0, m2, d2 / 12.0));
-    console.log(angle);
-    setAngle(angle);
-
-    robot1
-        .attr("cx", PIVOT.x + d1 * SCALE)
-        .attr("r", m1 * .1);
-    robot2
-        .attr("cx", PIVOT.x + d2 * SCALE)
-        .attr("r", m2 * .1);
-
-    text.text(`${Math.abs(angle).toFixed(2)}°`)
-        .style("fill", Math.abs(angle) > 8  ? "red" : "black");
+            .attr("transform", `rotate(${theta} ${PIVOT.x} ${PIVOT.y})`);
 }
 
 function update() {
-    let m1 = d3.select("input[name=robot_1]").node().value;
-    let d1 = d3.select("input[name=robot_1_d]").node().value;
-    let m2 = d3.select("input[name=robot_2]").node().value;
-    let d2 = d3.select("input[name=robot_2_d]").node().value;
+    let m_1 = d3.select("input[name=robot_1]").node().value;
+    let d_1 = d3.select("input[name=robot_1_d]").node().value;
+    let m_2 = d3.select("input[name=robot_2]").node().value;
+    let d_2 = d3.select("input[name=robot_2_d]").node().value;
+    let m_3 = d3.select("input[name=robot_3]").node().value;
+    let d_3 = d3.select("input[name=robot_3_d]").node().value;
 
-    let angle = rad_to_deg(getAngle(m1, d1 / 12.0, m2, d2 / 12.0));
-    console.log(angle);
+    let angle = rad_to_deg(getAngle(m_1, d_1 / 12.0, m_2, d_2 / 12.0, m_3, d_3 / 12.0));
     setAngle(angle);
 
     robot1.transition()
-        .attr("cx", PIVOT.x + d1 * SCALE)
-        .attr("r", m1 * .1);
+        .attr("cx", PIVOT.x + d_1 * SCALE)
+        .attr("r", m_1 * .1);
     robot2.transition()
-        .attr("cx", PIVOT.x + d2 * SCALE)
-        .attr("r", m2 * .1);
+        .attr("cx", PIVOT.x + d_2 * SCALE)
+        .attr("r", m_2 * .1);
+    robot3.transition()
+        .attr("cx", PIVOT.x + d_3 * SCALE)
+        .attr("r", m_3 * .1);
 
     text.text(`${Math.abs(angle).toFixed(2)}°`)
-        .style("fill", Math.abs(angle) > 8  ? "red" : "black");
-
-
+        .transition()
+            .duration(1000)
+            .style("fill", Math.abs(angle) > 8  ? "red" : "black");
 }
 
-init();
+update();
 d3.selectAll("input").on("change", update);
